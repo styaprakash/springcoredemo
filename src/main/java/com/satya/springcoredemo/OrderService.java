64e22f81d39ff64ca53ab85594dc38525bbce410
Satya Prakash <satya.mishra@weassist.co.in>
2026-07-13 23:00:37 +0530

    Learned the Field injection, setter injection using @Autowired annotation

--- src/main/java/com/satya/springcoredemo/OrderService.java
+++ src/main/java/com/satya/springcoredemo/OrderService.java
@@ -5,16 +5,25 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OrderService {
-
-     private PaymentService paymentService;
+//    @Autowired
+     private final PaymentService paymentService;
 
     // // Constructor injection
 
-    @Autowired
+//    @Autowired
      public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
      }
 
+
+    //setter method
+//    @Autowired
+//    public void setPaymentService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
+
+    //so now basically in Setter method, we wired the dependency of PaymentService with OrderService
+    // after the creation of objects first
     public void placeOrder() {
          paymentService.pay();
         System.out.println("Order placed successfully");
